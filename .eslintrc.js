f2dbb74c5c9f4cb9b40b382fcf6cf9d169b773e9
dionysus0214 <hiba0214@naver.com>
2025-05-01 23:03:46 +0900

    chore: 프로젝트 설정 파일 및 관련 패키지 업데이트

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,80 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+  parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+      },
+  plugins: ['@typescript-eslint', 'import'],
+  rules: {
+    '@typescript-eslint/func-call-spacing': ['error'],
+    'no-trailing-spaces': ['error'],
+    'eol-last': ['error'],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type',
+        ],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+    'object-curly-spacing': ['error', 'always'],
+        quotes: ['error', 'single', {avoidEscape: true}],
+      },
+  },
+  ],
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: './tsconfig.json',
+      },
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+      alias: {
+        map: [
+          ['@', './src'],
+          ['@styles', './src/styles'],
+          ['@components', './src/components'],
+          ['@screens', './src/screens'],
+          ['@utils', './src/utils'],
+          ['@hooks', './src/hooks'],
+          ['@assets', './src/assets']
+        ],
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json']
+      }
+    },
+  },
+  ignorePatterns: [
+    'node_modules/',
+    'build/',
+    'dist/',
+    'coverage/',
+    '.eslintcache',
+    '*.config.js',
+    '*.config.ts',
+  ],
 };
